26369084ae8d0334361aa47f73de99e0db833661
Soham Mondal <soham@triveous.com>
2015-08-03 16:46:08 +0530

    added seekbar

--- app/src/main/java/com/novopay/sohammusicplayer/MainActivity.java
+++ app/src/main/java/com/novopay/sohammusicplayer/MainActivity.java
@@ -2,11 +2,15 @@ package com.novopay.sohammusicplayer;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
@@ -15,21 +19,30 @@ public class MainActivity extends ActionBarActivity {
 
     private MediaPlayer mediaPlayer;
 
+    private SeekBar seekBar;
+
+    private MusicHandler musicHandler = new MusicHandler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mediaPlayer = MediaPlayer.create(this, R.raw.a);
+        mediaPlayer = MediaPlayer.create(this, R.raw.a);
 
         mPlayButton = (Button) findViewById(R.id.activity_main_play);
         mPauseButton = (Button) findViewById(R.id.activity_main_pause);
+        seekBar = (SeekBar) findViewById(R.id.activity_main_seekbar);
+
+        seekBar.setMax(mediaPlayer.getDuration());
 
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Play is clicked", Toast.LENGTH_SHORT).show();
                 mediaPlayer.start();
+                musicHandler.sendEmptyMessage(MESSAGE_WAKE_UP_AND_SEEK);
+                Log.d("MainActivity", "Starting position/Total Duration is: " + mediaPlayer.getCurrentPosition() + ", " + mediaPlayer.getDuration());
             }
         });
 
@@ -37,7 +50,27 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Pause is clicked", Toast.LENGTH_SHORT).show();
+                musicHandler.removeMessages(MESSAGE_WAKE_UP_AND_SEEK);
                 mediaPlayer.pause();
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser)
+                    mediaPlayer.seekTo(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
@@ -63,4 +96,27 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static int MESSAGE_WAKE_UP_AND_SEEK = 10;
+    public static int MESSAGE_WAKE_UP_AND_STOP = 11;
+
+    class MusicHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == MESSAGE_WAKE_UP_AND_SEEK) {
+                // update the seekbar
+                if (mediaPlayer != null) {
+                    if(mediaPlayer.isPlaying()) {
+                        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                        sendEmptyMessageDelayed(MESSAGE_WAKE_UP_AND_SEEK, 200);
+                    }
+                }
+            }
+
+            super.handleMessage(msg);
+        }
+    }
+
+
 }
